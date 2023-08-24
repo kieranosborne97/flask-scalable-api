b20de48ca5303fd7f04910b6b7ca1aaeba4858d6
klo <kieran@klodevelopments.com.au>
2023-08-24 20:54:14 +1000

    Third attempt at GH actions to EB pipeline

--- application.py
+++ application.py
@@ -1,20 +1,20 @@
 import os
 from flask_openapi3 import OpenAPI
 
-app = OpenAPI(__name__)
+application = OpenAPI(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def home():
     return 'Welcome to our flask api, enjoy your stay!'
 
 
 # TODO(swap to hyphen)
-@app.route('/health_check')
+@application.route('/health_check')
 def health_check():
     environment: str = os.environ.get('ENVIRONMENT') if os.environ.get('ENVIRONMENT') else 'localhost'
     return f'Health Check, the environment is {environment}'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
